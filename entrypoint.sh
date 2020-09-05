182cfa646537517b757c67c5ce796798acf716ec
avvppro <avvppro@gmail.com>
2020-09-05 23:01:18 +0300

    sed in artefact

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #sed Backend credentials for Frontend
-#sed -i 's BACKEND_IP $BE_IP g' 
-
-#Треба знайти місця в білді з підстановкою
+cd /var/www/localhost/htdocs/
+find . -type f -exec sed -i "s|https://dtapi.if.ua/api/|http://$BALANCER_IP/dtapi|g" {} +
 # This will exec the CMD (apache2 start) from Dockerfile.
 exec "$@"
